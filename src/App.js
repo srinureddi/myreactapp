18ed7b8fa61ae2f0e1036d2a151cbc474c2a311b
srinureddi <srinureddi@gmail.com>
2024-08-06 22:42:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Srini Test
+          Learn React with Srini Test Workflow
         </a>
       </header>
     </div>
